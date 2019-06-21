d27a0ec0f10fb0b4ab82de0a6275257a7731a744
DiNikOs <demon22odn08@rambler.ru>
2019-06-21 12:37:28 +0300

    HW4. Basic

---
+++ src/ru/geekbrain/HW/HW4/TwoSideLinkedList.java
@@ -0,0 +1,6 @@
+package ru.geekbrain.HW.HW4;
+
+public interface TwoSideLinkedList<E> extends LinkedList<E> {
+
+    void insertLast(E value);
+}
---
+++ src/ru/geekbrain/less4/datastructure/TwoSideLinkedList.java
@@ -0,0 +1,6 @@
+package ru.geekbrain.less4.datastructure;
+
+public interface TwoSideLinkedList<E> extends LinkedList<E> {
+
+    void insertLast(E value);
+}
