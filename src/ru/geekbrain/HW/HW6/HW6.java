bd0d5537104e47a7e9cedb0837ceb8cb842ff408
DiNikOs <demon22odn08@rambler.ru>
2019-06-26 13:04:26 +0300

    HW6. Basic

--- src/ru/geekbrain/HW/HW6/HW6.java
+++ src/ru/geekbrain/HW/HW6/HW6.java
@@ -1,8 +1,45 @@
 package ru.geekbrain.HW.HW6;
 
+import java.util.Random;
+
 public class HW6 {
 
-    public static void main(String[] args) {
+    //Tree<Integer> treeTest;
 
+    public static void main(String[] args) {
+        Tree<Integer> treeTest = new TreeImpl<>();
+        seeTree(treeTest, 5, -20, 20);
+        
+    }
+
+    public static void seeTree (Tree <Integer> treeTest, int deep, int min, int max) {
+       // int count = factorial(deep);
+        int count = deep;
+        for (int i = 0; i <= count; i++) {
+            treeTest.add(randomMinMax(min, max));
+        }
+        treeTest.display();
+        treeTest.traverse(Tree.TraverseMode.IN_ORDER);
+        System.out.println("---------------------------");
+        treeTest.traverse(Tree.TraverseMode.PRE_ORDER);
+        System.out.println("---------------------------");
+        treeTest.traverse(Tree.TraverseMode.POST_ORDER);
+
+
+    }
+
+    public static int randomMinMax (int min, int max) {
+        int diff = max - min;
+        Random random = new Random();
+        int i = random.nextInt(diff + 1);
+        i += min;
+        return i;
+    }
+
+    private static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
