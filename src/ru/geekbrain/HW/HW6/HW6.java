92d32c731922ba64b55b6c1d7ea2a9aa43623d48
DiNikOs <demon22odn08@rambler.ru>
2019-06-27 23:13:45 +0300

    HW6. Done

--- src/ru/geekbrain/HW/HW6/HW6.java
+++ src/ru/geekbrain/HW/HW6/HW6.java
@@ -7,9 +7,8 @@ public class HW6 {
     public static void main(String[] args) {
         int deep = 4;
         int treeCount = 20;
-        int diff = 50;
+        int diff = 25;
         seeTree(deep, diff, treeCount);
-
     }
 
     public static void seeTree(int deep, int diff, int treeCount) {
@@ -35,9 +34,10 @@ public class HW6 {
     }
 
     private static int randomMinMax(int diff) {
-            Random random = new Random();
-            int i = random.nextInt(diff + 1);
-            return (i -= diff/2);
-        }
+        Random random = new Random();
+        diff = diff*2;
+        int i = random.nextInt(diff + 1);
+        return (i -= diff/2);
     }
+}
 
